5649f97d7d7c6a6da5b44900eb71a599a9ec2fdb
Vinh Do <vinhdo07@gmail.com>
2019-02-28 04:11:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
-    self = Nokogiri::HTML::Document
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
   def print_courses
